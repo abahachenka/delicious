618f62cc805b2ce2fad341929fa801022c15c4d8
Lena Bogachenko <l.boga4enko@gmail.com>
2020-09-17 09:51:41 +0300

    Finished header

--- js/script.js
+++ js/script.js
@@ -10,20 +10,31 @@ $(document).ready(() => {
         const $pageHeaderMenu = $('.page-header__menu');
         const $body = $('body');
 
+        // toggle menu on burger click
         $pageHeaderBurger.click((e) => {
             $pageHeaderBurger.toggleClass('page-header__burger-active');
             $pageHeaderMenu.toggleClass('page-header__menu-active');
             $body.toggleClass('lock');
         });
 
+        // close the menu when user clicks outside the menu area
         $(document).mouseup(e => {
             if ($pageHeaderMenu.hasClass('page-header__menu-active')
+                && !$pageHeaderBurger.is(e.target)
                 && !$pageHeaderMenu.is(e.target) // if the target of the click isn't the container...
                 && $pageHeaderMenu.has(e.target).length === 0) {// ... nor a descendant of the container
                 
                 $pageHeaderMenu.removeClass('page-header__menu-active');
                 $pageHeaderBurger.removeClass('page-header__burger-active');
                 $body.removeClass('lock');
+            }
+        });
+
+        // close the menu when window leaves the mobile breakpoint
+        $(window).resize(() => {
+            if ($(window).width() >= 768 ) {
+                $pageHeaderMenu.removeClass('page-header__menu-active');
+                $pageHeaderBurger.removeClass('page-header__burger-active');
             }
         });
     }
