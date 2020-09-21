d82b7db12bca14de88d5d73b914db0ad20825f04
Lena Bogachenko <l.boga4enko@gmail.com>
2020-09-21 23:37:17 +0300

    Improved file structure

---
+++ src/components/page-header/page-header.js
@@ -0,0 +1,68 @@
+/**
+ * Initializes the top menu event handlers
+ */
+export function initHeaderMenu() {
+    const $pageHeaderBurger = $('.page-header__burger');
+    const $pageHeaderMenu = $('.page-header__menu');
+    const $body = $('body');
+
+    // toggle menu on burger click
+    $pageHeaderBurger.click((e) => {
+        $pageHeaderBurger.toggleClass('menu__icon-active');
+        $pageHeaderMenu.toggleClass('menu-active');
+        $body.toggleClass('lock');
+    });
+
+    // close the menu when user clicks outside the menu area
+    $(document).mouseup(e => {
+        if ($pageHeaderMenu.hasClass('menu-active')
+            && !$pageHeaderBurger.is(e.target)
+            && !$pageHeaderMenu.is(e.target) // if the target of the click isn't the container...
+            && $pageHeaderMenu.has(e.target).length === 0) {// ... nor a descendant of the container
+            
+            $pageHeaderMenu.removeClass('menu-active');
+            $pageHeaderBurger.removeClass('menu__icon-active');
+            $body.removeClass('lock');
+        }
+    });
+
+    // close the menu when window leaves the mobile breakpoint
+    $(window).resize(() => {
+        if ($(window).width() >= 768 ) {
+            $pageHeaderMenu.removeClass('menu-active');
+            $pageHeaderBurger.removeClass('menu__icon-active');
+        }
+    });
+}
+
+/**
+ * Initializes the search event handlers
+*/
+export function initHeaderSearch() {
+    const $searchContainer = $('.page-header__search');
+    const searchActiveClass = 'search_active';
+
+    $('.page-header__search-icon').click(e => {
+        let isActive = $searchContainer.hasClass(searchActiveClass);
+
+        if (!isActive) {
+            let $searchInput = $searchContainer.find('input')[0];
+
+            setTimeout(function() {
+                $searchInput.focus();
+            }, 100);
+        }
+        
+        $searchContainer.toggleClass(searchActiveClass);
+    });
+
+    // Close search panel on clicking outside the panel
+    $(document).mouseup(e => {
+        if ($searchContainer.hasClass(searchActiveClass)
+            && !$searchContainer.is(e.target) // if the target of the click isn't the container...
+            && $searchContainer.has(e.target).length === 0) {// ... nor a descendant of the container
+            
+            $searchContainer.removeClass(searchActiveClass);
+        }
+    });
+}
---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+import {initHeaderMenu, initHeaderSearch} from '../components/page-header/page-header.js';
+
+$(document).ready(() => {
+    initHeaderMenu();
+    initHeaderSearch();
+});
